2de83dd9c68051d3b8ce51e45433a5b26a4d34e1
NOMAD <gokhantosun_21@hotmail.com>
2022-06-17 00:37:54 +0300

    Update kur.py

--- kur.py
+++ kur.py
@@ -1,11 +1,12 @@
 import os
 import sys
-
-os.system("cd")
+print("Github dan veri indirme başlatılıyor...")
 os.system("git clone --depth=1 https://github.com/adi1090x/termux-desktop.git")
-os.system("cd termux-desktop")
+print("kullanım klasörü değiştiriliyor")
+os.chdir("/termux-desktop")
 os.system("chmod +x setup.sh")
 os.system("clear")
 input("Birazdan Termux desktop kurulumu başlayacak \n  ilk başta şifre girmeniz istenecek \n ardından şifreyi doğruladıktan sonra \n view-only sorusu gelecek ona \n n diyip devam edin \n işlemlere başlamak için 'Enter' basınız")
 os.system("clear")
+print("Kurulum işlemleri başlatılıyor...")
 os.system("./setup.sh --install")
