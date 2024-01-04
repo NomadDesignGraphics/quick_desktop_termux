f24f65c1b23b0733a68f7f770ff6dc9f3135a739
NOMAD <gokhantosun_21@hotmail.com>
2024-01-05 02:26:15 +0300

    görev taşıma

--- kur.py
+++ kur.py
@@ -1,12 +1,14 @@
 import os
 import sys
-print("Github dan veri indirme başlatılıyor...")
-os.system("git clone --depth=1 https://github.com/adi1090x/termux-desktop.git")
-print("kullanım klasörü değiştiriliyor")
-os.chdir("/termux-desktop")
-os.system("chmod +x setup.sh")
-os.system("clear")
-input("Birazdan Termux desktop kurulumu başlayacak \n  ilk başta şifre girmeniz istenecek \n ardından şifreyi doğruladıktan sonra \n view-only sorusu gelecek ona \n n diyip devam edin \n işlemlere başlamak için 'Enter' basınız")
-os.system("clear")
-print("Kurulum işlemleri başlatılıyor...")
-os.system("./setup.sh --install")
+
+def main():
+  print("Github dan veri indirme başlatılıyor...")
+  os.system("git clone --depth=1 https://github.com/adi1090x/termux-desktop.git")
+  print("kullanım klasörü değiştiriliyor")
+  os.chdir("/termux-desktop")
+  os.system("chmod +x setup.sh")
+  os.system("clear")
+  input("Birazdan Termux desktop kurulumu başlayacak \n  ilk başta şifre girmeniz istenecek \n ardından şifreyi doğruladıktan sonra \n view-only sorusu gelecek ona \n n diyip devam edin \n işlemlere başlamak için 'Enter' basınız")
+  os.system("clear")
+  print("Kurulum işlemleri başlatılıyor...")
+  os.system("./setup.sh --install")
